acd75c43e5fadabcad5f2798b37df9baf6ff236a
know <do.i.know.it@yggdrashill.com>
2020-11-26 12:29:53 +0900

    Add implementations for Signalization

---
+++ YggdrAshill.Nuadha/Signalization/Disconnection.cs
@@ -0,0 +1,42 @@
+using YggdrAshill.Nuadha.Signalization;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class Disconnection :
+        IDisconnection
+    {
+        private readonly Action onDisconnected;
+
+        #region Constructor
+
+        public Disconnection(Action onDisconnected)
+        {
+            if (onDisconnected == null)
+            {
+                throw new ArgumentNullException(nameof(onDisconnected));
+            }
+
+            this.onDisconnected = onDisconnected;
+        }
+
+        public Disconnection()
+        {
+            onDisconnected = () =>
+            {
+
+            };
+        }
+
+        #endregion
+
+        #region IDisconnection
+
+        public void Disconnect()
+        {
+            onDisconnected.Invoke();
+        }
+
+        #endregion
+    }
+}
