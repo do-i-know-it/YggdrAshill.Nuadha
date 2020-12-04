a7dad990a0eae0c322e4348209f3a0c64029b971
know <do.i.know.it@yggdrashill.com>
2020-12-04 10:17:05 +0900

    Add implementations for Unitization

---
+++ YggdrAshill.Nuadha/Unitization/SoftwareConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Unitization;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class SoftwareConfiguration<TSoftwareHandler> :
+        ISoftwareConfiguration<TSoftwareHandler>
+        where TSoftwareHandler : ISoftwareHandler
+    {
+        private readonly Func<TSoftwareHandler, IEmission> onConnected;
+
+        #region Constructor
+
+        public SoftwareConfiguration(Func<TSoftwareHandler, IEmission> onConnected)
+        {
+            if (onConnected == null)
+            {
+                throw new ArgumentNullException(nameof(onConnected));
+            }
+
+            this.onConnected = onConnected;
+        }
+
+        public SoftwareConfiguration()
+        {
+            onConnected = (_) =>
+            {
+                return new Emission();
+            };
+        }
+
+        #endregion
+
+        #region ISoftwareConfiguration
+
+        public IEmission Connect(TSoftwareHandler handler)
+        {
+            return onConnected.Invoke(handler);
+        }
+
+        #endregion
+    }
+}
