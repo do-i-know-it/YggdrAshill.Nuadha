5d1e15c8e345824a16a6decb837dffdff12a96fc
know <do.i.know.it@yggdrashill.com>
2020-12-08 20:23:18 +0900

    Add implementaions for Signals

---
+++ YggdrAshill.Nuadha/Signals/Push/PushEventSystem.cs
@@ -0,0 +1,77 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Operation;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class PushEventSystem :
+        IPushEventSystem
+    {
+        private readonly IConnector<Pulse> hasPushed = new Connector<Pulse>();
+
+        private readonly IConnector<Pulse> isPushed = new Connector<Pulse>();
+
+        private readonly IConnector<Pulse> hasReleased = new Connector<Pulse>();
+
+        private readonly IConnector<Pulse> isReleased = new Connector<Pulse>();
+
+        #region IPushEventHandler
+
+        public IOutputTerminal<Pulse> HasPushed => hasPushed;
+
+        public IOutputTerminal<Pulse> IsPushed => isPushed;
+
+        public IOutputTerminal<Pulse> HasReleased => hasReleased;
+
+        public IOutputTerminal<Pulse> IsReleased => isReleased;
+
+        #endregion
+
+        #region IConnection
+
+        public IDisconnection Connect(IOutputTerminal<Push> terminal)
+        {
+            if (terminal == null)
+            {
+                throw new ArgumentNullException(nameof(terminal));
+            }
+
+            var hasPushed = terminal.Pulsate(PushToPulse.HasPushed).Connect(this.hasPushed);
+
+            var isPushed = terminal.Pulsate(PushToPulse.IsPushed).Connect(this.isPushed);
+
+            var hasReleased = terminal.Pulsate(PushToPulse.HasReleased).Connect(this.hasReleased);
+
+            var isReleased = terminal.Pulsate(PushToPulse.IsReleased).Connect(this.isReleased);
+
+            return new Disconnection(() =>
+            {
+                hasPushed.Disconnect();
+
+                isPushed.Disconnect();
+
+                hasReleased.Disconnect();
+
+                isReleased.Disconnect();
+            });
+        }
+
+        #endregion
+
+        #region IDisconnection
+
+        public void Disconnect()
+        {
+            hasPushed.Disconnect();
+
+            isPushed.Disconnect();
+
+            hasReleased.Disconnect();
+
+            isReleased.Disconnect();
+        }
+
+        #endregion
+    }
+}
---
+++ YggdrAshill.Nuadha/Signals/Touch/TouchEventSystem.cs
@@ -0,0 +1,77 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Operation;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class TouchEventSystem :
+        ITouchEventSystem
+    {
+        private readonly IConnector<Pulse> hasTouched = new Connector<Pulse>();
+
+        private readonly IConnector<Pulse> isTouched = new Connector<Pulse>();
+        
+        private readonly IConnector<Pulse> hasReleased = new Connector<Pulse>();
+        
+        private readonly IConnector<Pulse> isReleased = new Connector<Pulse>();
+
+        #region ITouchEventHandler
+
+        public IOutputTerminal<Pulse> HasTouched => hasTouched;
+
+        public IOutputTerminal<Pulse> IsTouched => isTouched;
+
+        public IOutputTerminal<Pulse> HasReleased => hasReleased;
+
+        public IOutputTerminal<Pulse> IsReleased => isReleased;
+
+        #endregion
+
+        #region IConnection
+
+        public IDisconnection Connect(IOutputTerminal<Touch> terminal)
+        {
+            if (terminal == null)
+            {
+                throw new ArgumentNullException(nameof(terminal));
+            }
+
+            var hasTouched = terminal.Pulsate(TouchToPulse.HasTouched).Connect(this.hasTouched);
+
+            var isTouched = terminal.Pulsate(TouchToPulse.IsTouched).Connect(this.isTouched);
+            
+            var hasReleased = terminal.Pulsate(TouchToPulse.HasReleased).Connect(this.hasReleased);
+            
+            var isReleased = terminal.Pulsate(TouchToPulse.IsReleased).Connect(this.isReleased);
+
+            return new Disconnection(() =>
+            {
+                hasTouched.Disconnect();
+            
+                isTouched.Disconnect();
+                
+                hasReleased.Disconnect();
+                
+                isReleased.Disconnect();
+            });
+        }
+
+        #endregion
+
+        #region IDisconnection
+
+        public void Disconnect()
+        {
+            hasTouched.Disconnect();
+
+            isTouched.Disconnect();
+
+            hasReleased.Disconnect();
+
+            isReleased.Disconnect();
+        }
+
+        #endregion
+    }
+}
