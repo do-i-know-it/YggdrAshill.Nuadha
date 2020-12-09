faa8358a882acb13335b8b157081370a9c9d06d4
know <do.i.know.it@yggdrashill.com>
2020-12-10 00:54:22 +0900

    Add definitions for Conduction

---
+++ Conduction/IGenerator.cs
@@ -0,0 +1,13 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+
+namespace YggdrAshill.Nuadha.Conduction
+{
+    public interface IGenerator<TSignal> :
+        IOutputTerminal<TSignal>,
+        IIgnitor,
+        IDisconnection
+        where TSignal : ISignal
+    {
+
+    }
+}
