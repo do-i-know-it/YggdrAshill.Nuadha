0a197aac0a953e5bdc90ca056f7bc6b3e2ebe01e
know <do.i.know.it@yggdrashill.com>
2020-12-09 17:03:35 +0900

    Add implementations for Signals

---
+++ YggdrAshill.Nuadha.Specification/Signals/Position/PositionSystemSpecification.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Signals;
+
+namespace YggdrAshill.Nuadha.Specification
+{
+    [TestFixture(TestOf = typeof(PositionSystem))]
+    internal class PositionSystemSpecification
+    {
+        private IConnector<Position> connector;
+        private IOutputTerminal<Position> Terminal => connector;
+
+        private PositionSystem system;
+
+        private IDisconnection disconnection;
+
+        private readonly float offset = 1.0f;
+
+        [SetUp]
+        public void SetUp()
+        {
+            connector = new Connector<Position>();
+
+            system = new PositionSystem(new Position(offset, offset, offset));
+
+            disconnection = system.Connect(Terminal);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            connector.Disconnect();
+            connector = default;
+
+            system.Disconnect();
+            system = default;
+
+            disconnection.Disconnect();
+            disconnection = default;
+        }
+
+        [TestCase(0.0f, 0.0f, 1.0f)]
+        [TestCase(0.0f, 1.0f, 0.0f)]
+        [TestCase(1.0f, 0.0f, 0.0f)]
+        [TestCase(0.0f, 0.0f, 0.0f)]
+        public void ShouldReceiveCalibratedPosition(float horizontal, float vertical, float frontal)
+        {
+            var received = default(Position);
+            var terminal = new InputTerminal<Position>(signal =>
+            {
+                received = signal;
+            });
+
+            var disconnection = system.Connect(terminal);
+
+            var position = new Position(horizontal, vertical, frontal);
+
+            var expected = new Position(horizontal + offset, vertical + offset, frontal + offset);
+
+            connector.Receive(position);
+
+            Assert.AreEqual(expected, received);
+
+            disconnection.Disconnect();
+        }
+    }
+}
---
+++ YggdrAshill.Nuadha/Signals/Position/PositionSystem.cs
@@ -0,0 +1,56 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class PositionSystem :
+        IPositionSystem
+    {
+        private readonly IConnector<Position> connector = new Connector<Position>();
+
+        private readonly Position offset;
+
+        public PositionSystem(Position offset)
+        {
+            this.offset = offset;
+        }
+
+        #region IDivider
+
+        public IDisconnection Connect(IOutputTerminal<Position> terminal)
+        {
+            if (terminal == null)
+            {
+                throw new ArgumentNullException(nameof(terminal));
+            }
+
+            return terminal.Calibrate(PositionToPosition.Add, offset).Connect(connector);
+        }
+
+        #endregion
+
+        #region IOutputTerminal
+
+        public IDisconnection Connect(IInputTerminal<Position> terminal)
+        {
+            if (terminal == null)
+            {
+                throw new ArgumentNullException(nameof(terminal));
+            }
+
+            return connector.Connect(terminal);
+        }
+
+        #endregion
+
+        #region IDisconnection
+
+        public void Disconnect()
+        {
+            connector.Disconnect();
+        }
+
+        #endregion
+    }
+}
