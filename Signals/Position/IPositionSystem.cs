2850eaa30e7cfdd89fe5ca3464b5c26d4eea485a
know <do.i.know.it@yggdrashill.com>
2020-12-09 11:45:51 +0900

    Add definitions for Signals

---
+++ Signals/Position/IPositionSystem.cs
@@ -0,0 +1,13 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Conduction;
+
+namespace YggdrAshill.Nuadha.Signals
+{
+    public interface IPositionSystem :
+        IDivider<Position>,
+        IOutputTerminal<Position>,
+        IDisconnection
+    {
+
+    }
+}
