67ec6736817034a2efb201d01c38788b341dbfe6
know <do.i.know.it@yggdrashill.com>
2020-12-10 10:03:38 +0900

    Revert "Remove unnecessary implementations for Units"
    This reverts commit fe8467455970f6ed56ee85593a1015c6623c235f.

---
+++ YggdrAshill.Nuadha/Units/ThreePointTracker/ThreePointTrackerEventSystem.cs
@@ -0,0 +1,66 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Unitization;
+using YggdrAshill.Nuadha.Units;
+using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class ThreePointTrackerEventSystem :
+        ISoftware<IThreePointTrackerSoftwareHandler>,
+        IThreePointTrackerEventHandler,
+        IDisconnection
+    {
+        private readonly HandControllerEventSystem leftHand;
+        
+        private readonly HandControllerEventSystem rightHand;
+
+        public ThreePointTrackerEventSystem(HysteresisThreshold thumbStick, HysteresisThreshold fingerTrigger, HysteresisThreshold handTrigger)
+        {
+            leftHand = new HandControllerEventSystem(thumbStick, fingerTrigger, handTrigger);
+
+            rightHand = new HandControllerEventSystem(thumbStick, fingerTrigger, handTrigger);
+        }
+
+        #region IThreePointTrackerEventHandler
+
+        public IHandControllerEventHandler LeftHand => leftHand;
+
+        public IHandControllerEventHandler RightHand => rightHand;
+
+        #endregion
+
+        #region ISoftware
+
+        public IDisconnection Connect(IThreePointTrackerSoftwareHandler handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            var leftHand = this.leftHand.Connect(handler.LeftHand);
+
+            var rightHand = this.rightHand.Connect(handler.RightHand);
+
+            return new Disconnection(() =>
+            {
+                leftHand.Disconnect();
+
+                rightHand.Disconnect();
+            });
+        }
+
+        #endregion
+
+        #region IDisconnection
+
+        public void Disconnect()
+        {
+            leftHand.Disconnect();
+
+            rightHand.Disconnect();
+        }
+
+        #endregion
+    }
+}
