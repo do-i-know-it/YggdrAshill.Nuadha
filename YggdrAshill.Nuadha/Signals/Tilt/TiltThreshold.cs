c492c068630f3b9490cac4b3ceac2a8bef196e23
know <do.i.know.it@yggdrashill.com>
2020-12-15 17:58:55 +0900

    Add implementations for Signals

---
+++ YggdrAshill.Nuadha/Signals/Tilt/TiltThreshold.cs
@@ -0,0 +1,29 @@
+﻿namespace YggdrAshill.Nuadha
+{
+    public sealed class TiltThreshold :
+        ITiltThreshold
+    {
+        public HysteresisThreshold Center { get; }
+
+        public HysteresisThreshold Left { get; }
+
+        public HysteresisThreshold Right { get; }
+
+        public HysteresisThreshold Forward { get; }
+
+        public HysteresisThreshold Backward { get; }
+
+        public TiltThreshold(HysteresisThreshold threshold)
+        {
+            Center = threshold;
+
+            Left = threshold;
+
+            Right = threshold;
+
+            Forward = threshold;
+
+            Backward = threshold;
+        }
+    }
+}
