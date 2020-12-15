a1a6553cfcde197e2ef3322909c87df40df33ab3
know <do.i.know.it@yggdrashill.com>
2020-12-15 17:59:11 +0900

    Add implementations for Units

---
+++ YggdrAshill.Nuadha/Units/Headset/HeadsetThreshold.cs
@@ -0,0 +1,17 @@
+﻿namespace YggdrAshill.Nuadha
+{
+    public sealed class HeadsetThreshold :
+        IHeadsetThreshold
+    {
+        public IEyeTrackerThreshold LeftEye { get; }
+
+        public IEyeTrackerThreshold RightEye { get; }
+
+        public HeadsetThreshold(HysteresisThreshold pupil, HysteresisThreshold blink)
+        {
+            LeftEye = new EyeTrackerThreshold(pupil, blink);
+
+            RightEye = new EyeTrackerThreshold(pupil, blink);
+        }
+    }
+}
---
+++ YggdrAshill.Nuadha/Units/ThreePointTracker/ThreePointTrackerThreshold.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace YggdrAshill.Nuadha
+{
+    public sealed class ThreePointTrackerThreshold :
+        IThreePointTrackerThreshold
+    {
+        public IHeadsetThreshold Head { get; }
+
+        public IHandControllerThreshold LeftHand { get; }
+
+        public IHandControllerThreshold RightHand { get; }
+
+        #region Constructor
+
+        public ThreePointTrackerThreshold(
+            HysteresisThreshold pupil,
+            HysteresisThreshold blink, 
+            HysteresisThreshold thumbStick, 
+            HysteresisThreshold fingerTrigger,
+            HysteresisThreshold handTrigger)
+        {
+            Head = new HeadsetThreshold(pupil, blink);
+
+            LeftHand = new HandControllerThreshold(thumbStick, fingerTrigger, handTrigger);
+
+            RightHand = new HandControllerThreshold(thumbStick, fingerTrigger, handTrigger);
+        }
+
+        public ThreePointTrackerThreshold(
+            IHeadsetThreshold head, 
+            IHandControllerThreshold leftHand, 
+            IHandControllerThreshold rightHand)
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+            if (leftHand == null)
+            {
+                throw new ArgumentNullException(nameof(leftHand));
+            }
+            if (rightHand == null)
+            {
+                throw new ArgumentNullException(nameof(rightHand));
+            }
+
+            Head = head;
+            
+            LeftHand = leftHand;
+            
+            RightHand = rightHand;
+        }
+
+        #endregion
+    }
+}
