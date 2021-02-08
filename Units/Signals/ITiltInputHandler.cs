bb4d7a1452dd763deede8050f71b15c7aa7b777c
know <do.i.know.it@yggdrashill.com>
2021-02-08 23:27:11 +0900

    Add definitions for Units

---
+++ Units/Signals/ITiltInputHandler.cs
@@ -0,0 +1,16 @@
+﻿using YggdrAshill.Nuadha.Unitization;
+
+namespace YggdrAshill.Nuadha.Units
+{
+    public interface ITiltInputHandler :
+        IHardwareHandler
+    {
+        IPulseInputHandler Left { get; }
+
+        IPulseInputHandler Right { get; }
+
+        IPulseInputHandler Forward { get; }
+
+        IPulseInputHandler Backward { get; }
+    }
+}
---
+++ Units/Signals/ITiltOutputHandler.cs
@@ -0,0 +1,16 @@
+﻿using YggdrAshill.Nuadha.Unitization;
+
+namespace YggdrAshill.Nuadha.Units
+{
+    public interface ITiltOutputHandler :
+        ISoftwareHandler
+    {
+        IPulseOutputHandler Left { get; }
+
+        IPulseOutputHandler Right { get; }
+
+        IPulseOutputHandler Forward { get; }
+
+        IPulseOutputHandler Backward { get; }
+    }
+}
