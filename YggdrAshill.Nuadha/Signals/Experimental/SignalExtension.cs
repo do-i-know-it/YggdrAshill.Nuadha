fb27759a58c76635c2ca5f46f6d21868f8e45675
know <do.i.know.it@yggdrashill.com>
2021-02-08 22:59:30 +0900

    Add extension for Signals

---
+++ YggdrAshill.Nuadha/Signals/Experimental/SignalExtension.cs
@@ -0,0 +1,66 @@
+﻿using YggdrAshill.Nuadha.Signalization;
+using YggdrAshill.Nuadha.Conduction;
+using YggdrAshill.Nuadha.Translation;
+using YggdrAshill.Nuadha.Translation.Experimental;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha.Experimental
+{
+    public static class SignalExtension
+    {
+        #region Position
+
+        public static IConnection<Position> Calibrate(this IConnection<Position> connection, Func<Position> calibration)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            if (calibration == null)
+            {
+                throw new ArgumentNullException(nameof(calibration));
+            }
+
+            return connection.Calibrate(Signals.Calibrate.Position, new Calibration<Position>(calibration));
+        }
+
+        #endregion
+
+        #region Rotation
+
+        public static IConnection<Rotation> Calibrate(this IConnection<Rotation> connection, Func<Rotation> calibration)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            if (calibration == null)
+            {
+                throw new ArgumentNullException(nameof(calibration));
+            }
+
+            return connection.Calibrate(Signals.Calibrate.Rotation, new Calibration<Rotation>(calibration));
+        }
+
+        #endregion
+
+        #region Direction
+
+        public static IConnection<Direction> Calibrate(this IConnection<Direction> connection, Func<Direction> calibration)
+        {
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            if (calibration == null)
+            {
+                throw new ArgumentNullException(nameof(calibration));
+            }
+
+            return connection.Calibrate(Signals.Calibrate.Direction, new Calibration<Direction>(calibration));
+        }
+
+        #endregion
+    }
+}
