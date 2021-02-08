dbbae3bb486790cbb07f3491df513c48f8f48463
know <do.i.know.it@yggdrashill.com>
2021-02-08 23:00:20 +0900

    Add definitions for calibration

---
+++ YggdrAshill.Nuadha/Units/Translation/Experimental/PositionCalibrationSystem.cs
@@ -0,0 +1,40 @@
+﻿using YggdrAshill.Nuadha.Conduction;
+using YggdrAshill.Nuadha.Translation;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha.Experimental
+{
+    public sealed class PositionCalibrationSystem : CalibrationSystem<Position>
+    {
+        protected override IPropagation<Position> Propagation { get; } = new Propagation<Position>();
+
+        protected override IReduction<Position> Reduction { get; } = Calibrate.Position;
+
+        protected override ICalibration<Position> Calibration { get; }
+
+        #region Constructor
+
+        public PositionCalibrationSystem(ICalibration<Position> calibration)
+        {
+            if (calibration == null)
+            {
+                throw new ArgumentNullException(nameof(calibration));
+            }
+
+            Calibration = calibration;
+        }
+
+        public PositionCalibrationSystem(Func<Position> calibration)
+        {
+            if (calibration == null)
+            {
+                throw new ArgumentNullException(nameof(calibration));
+            }
+
+            Calibration = new Calibration<Position>(calibration);
+        }
+
+        #endregion
+    }
+}
