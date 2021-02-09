9cfcb4631c0b5dcdb4fa51a1105a87f144b013db
know <do.i.know.it@yggdrashill.com>
2021-02-09 16:07:48 +0900

    Add definitions for Signals

---
+++ YggdrAshill.Nuadha.Specification/Signals/MemoryUsageSpecification.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using YggdrAshill.Nuadha.Signals;
+using System;
+
+namespace YggdrAshill.Nuadha.Specification
+{
+    [TestFixture(TestOf = typeof(MemoryUsage))]
+    internal class MemoryUsageSpecification
+    {
+        [TestCase(0L, 1L)]
+        [TestCase(1L, 0L)]
+        [TestCase(0L, 0L)]
+        public void UsedSizeAndUnusedSizeShouldBeEqualToTotalSize(long used, long unused)
+        {
+            var expected = used + unused;
+
+            Assert.AreEqual(expected, new MemoryUsage(used, unused).TotalSize);
+        }
+
+        [Test]
+        public void CannotBeGeneratedWithUsedSizeLowerThanZero()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var usage = new MemoryUsage(-1L, 0L);
+            });
+        }
+
+        [Test]
+        public void CannotBeGeneratedWithUnusedSizeLowerThanZero()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var usage = new MemoryUsage(0L, -1L);
+            });
+        }
+    }
+}
