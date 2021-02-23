1d59043abf4f1942446c814c3ec177072cc00a2f
know <do.i.know.it@yggdrashill.com>
2021-02-23 23:49:47 +0900

    Move definitions for Unitization

--- YggdrAshill.Nuadha/Units/IInputHardware.cs
+++ YggdrAshill.Nuadha/Units/IInputHardware.cs
@@ -1,7 +1,8 @@
 using YggdrAshill.Nuadha.Signalization;
 using YggdrAshill.Nuadha.Conduction;
+using YggdrAshill.Nuadha.Unitization;
 
-namespace YggdrAshill.Nuadha.Unitization
+namespace YggdrAshill.Nuadha
 {
     public interface IInputHardware<TSystem> :
         IHardware<TSystem>,
