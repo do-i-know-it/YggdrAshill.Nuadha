34f09c3efa9461a2ae3d401f5db30dd91e11eb83
know <do.i.know.it@yggdrashill.com>
2021-05-14 10:58:21 +0900

    Add implementaions for Transformation

---
+++ YggdrAshill.Nuadha.Specification/Experimental/Transformation/Correction/CorrectionSpecification.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace YggdrAshill.Nuadha.Experimental.Specification
+{
+    [TestFixture(TestOf = typeof(Correction<>))]
+    internal class CorrectionSpecification
+    {
+        [Test]
+        public void ShouldExecuteFunctionWhenHasCorrected()
+        {
+            var expected = false;
+            var correction = new Correction<Signal>(signal =>
+            {
+                if (signal == null)
+                {
+                    throw new ArgumentNullException(nameof(signal));
+                }
+
+                expected = true;
+
+                return signal;
+            });
+
+            var corrected = correction.Correct(new Signal());
+
+            Assert.IsTrue(expected);
+        }
+
+        [Test]
+        public void CannotBeGeneratedWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var correction = new Correction<Signal>(null);
+            });
+        }
+    }
+}
---
+++ YggdrAshill.Nuadha.Specification/Experimental/Transformation/InputSignal.cs
@@ -0,0 +1,10 @@
+using YggdrAshill.Nuadha.Signalization.Experimental;
+
+namespace YggdrAshill.Nuadha.Experimental.Specification
+{
+    internal sealed class InputSignal :
+        ISignal
+    {
+
+    }
+}
