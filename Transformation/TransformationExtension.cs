49287596a20069bb95ce34b158931528981da89e
know <do.i.know.it@yggdrashill.com>
2021-07-09 17:15:37 +0900

    Add document comments for Transformation

--- Transformation/TransformationExtension.cs
+++ Transformation/TransformationExtension.cs
@@ -3,8 +3,35 @@ using System;
 
 namespace YggdrAshill.Nuadha.Transformation
 {
+    /// <summary>
+    /// Defines extensions for Transformation.
+    /// </summary>
     public static class TransformationExtension
     {
+        /// <summary>
+        /// Converts <typeparamref name="TInput"/> into <typeparamref name="TOutput"/>.
+        /// </summary>
+        /// <typeparam name="TInput">
+        /// Type of <see cref="ISignal"/> to convert.
+        /// </typeparam>
+        /// <typeparam name="TOutput">
+        /// Type of <see cref="ISignal"/> converted.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TInput}"/> to convert.
+        /// </param>
+        /// <param name="conversion">
+        /// <see cref="IConversion{TInput, TOutput}"/> to convert.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{TOutput}"/> converted.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="conversion"/> is null.
+        /// </exception>
         public static IProduction<TOutput> Convert<TInput, TOutput>(this IProduction<TInput> production, IConversion<TInput, TOutput> conversion)
             where TInput : ISignal
             where TOutput : ISignal
@@ -21,6 +48,27 @@ namespace YggdrAshill.Nuadha.Transformation
             return new Converter<TInput, TOutput>(production, conversion);
         }
 
+        /// <summary>
+        /// Corrects <typeparamref name="TSignal"/> with <see cref="Func{TSignal, TSignal}"/>.
+        /// </summary>
+        /// <typeparam name="TSignal">
+        /// Type of <see cref="ISignal"/> to correct.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TSignal}"/> to correct.
+        /// </param>
+        /// <param name="correction">
+        /// <see cref="ICorrection{TSignal}"/> to correct.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{TSignal}"/> corrected.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="correction"/> is null.
+        /// </exception>
         public static IProduction<TSignal> Convert<TSignal>(this IProduction<TSignal> production, ICorrection<TSignal> correction)
             where TSignal : ISignal
         {
@@ -36,6 +84,27 @@ namespace YggdrAshill.Nuadha.Transformation
             return production.Convert(new Correct<TSignal>(correction));
         }
 
+        /// <summary>
+        /// Converts <typeparamref name="TSignal"/> into <see cref="Pulse"/> with <see cref="Func{TSignal, Pulse}"/>.
+        /// </summary>
+        /// <typeparam name="TSignal">
+        /// Type of <see cref="ISignal"/> to convert.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TSignal}"/> to convert.
+        /// </param>
+        /// <param name="pulsation">
+        /// <see cref="IPulsation{TSignal}"/> to convert.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{Pulse}"/> converted.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="pulsation"/> is null.
+        /// </exception>
         public static IProduction<Pulse> Convert<TSignal>(this IProduction<TSignal> production, IPulsation<TSignal> pulsation)
             where TSignal : ISignal
         {
--- YggdrAshill.Nuadha/Transformation/TransformationExtension.cs
+++ YggdrAshill.Nuadha/Transformation/TransformationExtension.cs
@@ -4,8 +4,35 @@ using System;
 
 namespace YggdrAshill.Nuadha
 {
+    /// <summary>
+    /// Defines extensions for Transformation.
+    /// </summary>
     public static class TransformationExtension
     {
+        /// <summary>
+        /// Converts <typeparamref name="TInput"/> into <typeparamref name="TOutput"/> with <see cref="Func{TInput, TOutput}"/>.
+        /// </summary>
+        /// <typeparam name="TInput">
+        /// Type of <see cref="ISignal"/> to convert.
+        /// </typeparam>
+        /// <typeparam name="TOutput">
+        /// Type of <see cref="ISignal"/> converted.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TInput}"/> to convert.
+        /// </param>
+        /// <param name="conversion">
+        /// <see cref="Func{TInput, TOutput}"/> to convert.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{TOutput}"/> converted.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="conversion"/> is null.
+        /// </exception>
         public static IProduction<TOutput> Convert<TInput, TOutput>(this IProduction<TInput> production, Func<TInput, TOutput> conversion)
             where TInput : ISignal
             where TOutput : ISignal
@@ -22,6 +49,27 @@ namespace YggdrAshill.Nuadha
             return production.Convert(new Conversion<TInput, TOutput>(conversion));
         }
 
+        /// <summary>
+        /// Corrects <typeparamref name="TSignal"/> with <see cref="Func{TSignal, TSignal}"/>.
+        /// </summary>
+        /// <typeparam name="TSignal">
+        /// Type of <see cref="ISignal"/> to correct.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TSignal}"/> to correct.
+        /// </param>
+        /// <param name="correction">
+        /// <see cref="Func{TSignal, TSignal}"/> to correct.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{TSignal}"/> corrected.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="correction"/> is null.
+        /// </exception>
         public static IProduction<TSignal> Convert<TSignal>(this IProduction<TSignal> production, Func<TSignal, TSignal> correction)
             where TSignal : ISignal
         {
@@ -37,6 +85,27 @@ namespace YggdrAshill.Nuadha
             return production.Convert(new Correction<TSignal>(correction));
         }
 
+        /// <summary>
+        /// Converts <typeparamref name="TSignal"/> into <see cref="Pulse"/> with <see cref="Func{TSignal, Pulse}"/>.
+        /// </summary>
+        /// <typeparam name="TSignal">
+        /// Type of <see cref="ISignal"/> to convert.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TSignal}"/> to convert.
+        /// </param>
+        /// <param name="pulsation">
+        /// <see cref="Func{TSignal, Pulse}"/> to convert.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{Pulse}"/> converted.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="pulsation"/> is null.
+        /// </exception>
         public static IProduction<Pulse> Convert<TSignal>(this IProduction<TSignal> production, Func<TSignal, Pulse> pulsation)
             where TSignal : ISignal
         {
@@ -52,6 +121,27 @@ namespace YggdrAshill.Nuadha
             return production.Convert(new Pulsation<TSignal>(pulsation));
         }
 
+        /// <summary>
+        /// Detects <see cref="Notice"/> from <typeparamref name="TSignal"/> with <see cref="Func{TSignal, bool}"/>.
+        /// </summary>
+        /// <typeparam name="TSignal">
+        /// Type of <see cref="ISignal"/> to detect.
+        /// </typeparam>
+        /// <param name="production">
+        /// <see cref="IProduction{TSignal}"/> to detect.
+        /// </param>
+        /// <param name="detection">
+        /// <see cref="Func{TSignal, bool}"/> to detect.
+        /// </param>
+        /// <returns>
+        /// <see cref="IProduction{Notice}"/> detected.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="production"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="detection"/> is null.
+        /// </exception>
         public static IProduction<Notice> Detect<TSignal>(this IProduction<TSignal> production, Func<TSignal, bool> detection)
             where TSignal : ISignal
         {
@@ -65,6 +155,38 @@ namespace YggdrAshill.Nuadha
             }
 
             return production.Detect(detection);
+        }
+
+        /// <summary>
+        /// Produces with <see cref="Action"/> instead of <see cref="IConsumption{Notice}"/>.
+        /// </summary>
+        /// <param name="production">
+        /// <see cref="IProduction{Notice}"/> to produce.
+        /// </param>
+        /// <param name="consumption">
+        /// <see cref="Action"/> to execute when this has consumed <see cref="Notice"/>.
+        /// </param>
+        /// <returns></returns>
+        public static ICancellation Produce(this IProduction<Notice> production, Action consumption)
+        {
+            if (production == null)
+            {
+                throw new ArgumentNullException(nameof(production));
+            }
+            if (consumption == null)
+            {
+                throw new ArgumentNullException(nameof(consumption));
+            }
+
+            return production.Produce(new Consumption<Notice>(signal =>
+            {
+                if (signal == null)
+                {
+                    throw new ArgumentNullException(nameof(signal));
+                }
+
+                consumption.Invoke();
+            }));
         }
     }
 }
