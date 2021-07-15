2444eb0c5743c9ae3217342c1e265da183976896
know <do.i.know.it@yggdrashill.com>
2021-07-15 12:54:32 +0900

    Add specifications for Signalization

---
+++ YggdrAshill.Nuadha.Specification/Signalization/GenerationSpecification.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace YggdrAshill.Nuadha.Specification
+{
+    [TestFixture(TestOf = typeof(Generation))]
+    internal class GenerationSpecification
+    {
+        [Test]
+        public void ShouldExecuteFunctionWhenHasGenerated()
+        {
+            var expected = false;
+            var generation = Generation.Of(() =>
+            {
+                expected = true;
+
+                return new Signal();
+            });
+
+            generation.Generate();
+
+            Assert.IsTrue(expected);
+        }
+
+        [Test]
+        public void ShouldGenerateSignal()
+        {
+            var expected = new Signal();
+            var generation = Generation.Of(() =>
+            {
+                return expected;
+            });
+
+            var generated = generation.Generate();
+
+            Assert.AreEqual(expected, generated);
+        }
+
+        [Test]
+        public void CannotBeGeneratedWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var generation = Generation.Of<Signal>(null);
+            });
+        }
+    }
+}
---
+++ YggdrAshill.Nuadha.Specification/Signalization/SignalizationExtensionSpecification.cs
@@ -0,0 +1,135 @@
+using NUnit.Framework;
+using YggdrAshill.Nuadha.Signalization;
+using System;
+
+namespace YggdrAshill.Nuadha.Specification
+{
+    [TestFixture(TestOf = typeof(SignalizationExtension))]
+    internal class SignalizationExtensionSpecification
+    {
+        private Signal expected;
+
+        private IPropagation<Signal> propagation;
+
+        private SynthesizedCancellation synthesized;
+
+        [SetUp]
+        public void SetUp()
+        {
+            expected = new Signal();
+
+            propagation = Propagation.WithoutCache.Create<Signal>();
+
+            synthesized = new SynthesizedCancellation();
+        }
+
+        [Test]
+        public void ShouldProduceSignal()
+        {
+            var consumed = default(Signal);
+            var cancellation = propagation.Produce(signal =>
+            {
+                if (signal == null)
+                {
+                    throw new ArgumentNullException(nameof(signal));
+                }
+
+                consumed = signal;
+            });
+
+            propagation.Consume(expected);
+
+            Assert.AreEqual(expected, consumed);
+
+            cancellation.Cancel();
+        }
+
+        [Test]
+        public void ShouldTransmitSignal()
+        {
+            var transmission = propagation.Transmit(() =>
+            {
+                return expected;
+            });
+
+            var consumed = default(Signal);
+            var cancellation = transmission.Produce(signal =>
+            {
+                if (signal == null)
+                {
+                    throw new ArgumentNullException(nameof(signal));
+                }
+
+                consumed = signal;
+            });
+
+            transmission.Emit();
+
+            Assert.AreEqual(expected, consumed);
+
+            cancellation.Cancel();
+        }
+
+        [Test]
+        public void ShouldSynthesizeCancellation()
+        {
+            var synthesized = new SynthesizedCancellation();
+
+            var expected = false;
+            var cancellation = Cancellation.Of(() =>
+            {
+                expected = true;
+            });
+            cancellation.Synthesize(synthesized);
+
+            synthesized.Cancel();
+
+            Assert.IsTrue(expected);
+        }
+
+        [Test]
+        public void CannotProduceWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var cancellation = default(IPropagation<Signal>).Produce(_ => { });
+            });
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var cancellation = propagation.Produce(default(Action<Signal>));
+            });
+        }
+
+        [Test]
+        public void CannotTransmitWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var transmission = default(IPropagation<Signal>).Transmit(() =>
+                {
+                    return expected;
+                });
+            });
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var transmission = propagation.Transmit(default(Func<Signal>));
+            });
+        }
+
+        [Test]
+        public void CannotSynthesizeWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                default(ICancellation).Synthesize(synthesized);
+            });
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Cancellation.None.Synthesize(default(SynthesizedCancellation));
+            });
+        }
+    }
+}
