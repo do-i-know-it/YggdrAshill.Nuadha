8cd4242d344327cad1564d0fd735a864aad30f65
know <do.i.know.it@yggdrashill.com>
2021-10-28 16:20:31 +0900

    Modify implementations for Transformation

--- Transformation/PulseIs.cs
+++ Transformation/PulseIs.cs
@@ -3,41 +3,43 @@ using System;
 namespace YggdrAshill.Nuadha.Transformation
 {
     /// <summary>
-    /// Implementation of <see cref="ICondition{TSignal}"/> for <see cref="Pulse"/>.
+    /// Defines implementations of <see cref="ICondition{TSignal}"/> for <see cref="Pulse"/>.
     /// </summary>
-    public sealed class PulseIs :
-        ICondition<Pulse>
+    public static class PulseIs
     {
         /// <summary>
         /// When <see cref="Pulse"/> is <see cref="Pulse.HasDisabled"/> or <see cref="Pulse.IsDisabled"/>.
         /// </summary>
-        public static PulseIs Disabled { get; } = new PulseIs(Pulse.IsDisabled, Pulse.HasDisabled);
+        public static ICondition<Pulse> Disabled { get; } = new Condition(Pulse.IsDisabled, Pulse.HasDisabled);
 
         /// <summary>
         /// When <see cref="Pulse"/> is <see cref="Pulse.HasEnabled"/> or <see cref="Pulse.IsEnabled"/>.
         /// </summary>
-        public static PulseIs Enabled { get; } = new PulseIs(Pulse.IsEnabled, Pulse.HasEnabled);
-
-        private readonly Pulse first;
-
-        private readonly Pulse second;
-
-        private PulseIs(Pulse first, Pulse second)
+        public static ICondition<Pulse> Enabled { get; } = new Condition(Pulse.IsEnabled, Pulse.HasEnabled);
+        
+        private sealed class Condition :
+            ICondition<Pulse>
         {
-            this.first = first;
+            private readonly Pulse first;
 
-            this.second = second;
-        }
+            private readonly Pulse second;
 
-        /// <inheritdoc/>
-        public bool IsSatisfiedBy(Pulse signal)
-        {
-            if (signal == null)
+            internal Condition(Pulse first, Pulse second)
             {
-                throw new ArgumentNullException(nameof(signal));
+                this.first = first;
+
+                this.second = second;
             }
 
-            return signal == first || signal == second;
+            public bool IsSatisfiedBy(Pulse signal)
+            {
+                if (signal == null)
+                {
+                    throw new ArgumentNullException(nameof(signal));
+                }
+
+                return signal == first || signal == second;
+            }
         }
     }
 }
