4806c05b6d8468fe6af4614bf245d607f496815a
know <do.i.know.it@yggdrashill.com>
2021-11-01 12:50:48 +0900

    Fix document comments for Conduction

--- Conduction/IIgnition.cs
+++ Conduction/IIgnition.cs
@@ -1,14 +1,13 @@
-using YggdrAshill.Nuadha.Signalization;
 using YggdrAshill.Nuadha.Unitization;
 using System;
 
 namespace YggdrAshill.Nuadha.Conduction
 {
     /// <summary>
-    /// Connects <typeparamref name="TModule"/> to emit some types of <see cref="ISignal"/>.
+    /// Connects <typeparamref name="TModule"/> to emit.
     /// </summary>
     /// <typeparam name="TModule">
-    /// Type of <see cref="IModule"/> to ignite.
+    /// Type of <see cref="IModule"/> to connect.
     /// </typeparam>
     public interface IIgnition<TModule> :
         IConnection<TModule>,
