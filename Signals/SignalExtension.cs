2b2dbbc65aaf08459d3f9b71cf0af7116be91a2b
know <do.i.know.it@yggdrashill.com>
2021-11-17 16:46:37 +0900

    Modify specification of HysterisisThreshold

--- Signals/SignalExtension.cs
+++ Signals/SignalExtension.cs
@@ -217,5 +217,46 @@ namespace YggdrAshill.Nuadha
         }
 
         #endregion
+
+        #region Battery
+
+        /// <summary>
+        /// Converts <see cref="float"/> to <see cref="Battery"/>.
+        /// </summary>
+        /// <param name="signal">
+        /// <see cref="float"/> to covert.
+        /// </param>
+        /// <returns>
+        /// <see cref="Battery"/> converted.
+        /// </returns>
+        public static Battery ToBattery(this float signal)
+        {
+            if (signal < Battery.Empty)
+            {
+                signal = Battery.Empty;
+            }
+            if (signal > Battery.Full)
+            {
+                signal = Battery.Full;
+            }
+
+            return (Battery)signal;
+        }
+
+        /// <summary>
+        /// Converts <see cref="Battery"/> to <see cref="float"/>.
+        /// </summary>
+        /// <param name="signal">
+        /// <see cref="Battery"/> to covert.
+        /// </param>
+        /// <returns>
+        /// <see cref="float"/> converted.
+        /// </returns>
+        public static float ToSingle(this Battery signal)
+        {
+            return (float)signal;
+        }
+
+        #endregion
     }
 }
